b0f02087cab47081fc1f8efa07e3fcd19c7fc0b7
thesuss <susanna.larsdotter@gmail.com>
2016-01-21 11:17:50 +0000

    Lösta problem

--- my_group.rb
+++ my_group.rb
@@ -5,15 +5,15 @@ person_1 = {:namn=>"Thomas", :sex=>"male", :age=>44}
 person_2 = {:namn=>"Thesuss", :sex=>"female", :age=>30}     
 person_3 = {:namn=>"TheFajst", :sex=>"male", :age=>8}    
 
-#funkade innan
-my_group.push person_1
-my_group.push person_2
-my_group.push person_3
+#funkar typ
+my_group.push person_1, person_2, person_3
 
-
-my_group.each do |name|
- puts "#{person_1[:namn]} is a #{person_1[:age]} years old #{person_1[:sex]}"
+#funkar inte
+my_group.each do |meep|
+ puts "#{meep[:namn]} is a #{meep[:age]} years old #{meep[:sex]}"
 end
+
+
 
 #my_group.each do |name|
 #  puts "Hello #{key}"
